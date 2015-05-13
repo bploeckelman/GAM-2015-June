de3576ed78f5e321f14f5efa5e6eb2e9a6d45040
Colin Kennedy <moshen.colin@gmail.com>
2015-05-12 20:56:47 -0500

    Fix travis deploy script, update cache settings
    - Remove Ludumdare32 references
    - Make the deploy.sh script generic
    - Use bash from the environment
    - Remove registry.bin.lock file before travis caching

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,9 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+project="$(basename $(pwd))"
 
 mkdir deploy
 cp -r html/build/dist/* deploy/
-cp desktop/build/libs/desktop-*.jar deploy/LudumDare32.jar
+cp desktop/build/libs/desktop-*.jar "deploy/${project}.jar"
+
 cd deploy
-tar -czf ../LudumDare32.tar.gz ./
+# Tarball
+# tar -czf "../${project}.tar.gz" ./
+
+# Zip
+zip -r "../${project}.zip" ./
 cd ..
-curl -F filedata=@LudumDare32.tar.gz -H "$MOSHEN_UPLOAD_KEY" http://moshen.net/LudumDare32/deploy/
+
+# TODO: Upload the build somewhere; bintray maybe?
+# curl -T "${project}.zip" -uuser:<API_KEY> https://api.bintray.com/content/user/generic/<YOUR_COOL_PACKAGE_NAME>/<VERSION_NAME>/<FILE_TARGET_PATH>
