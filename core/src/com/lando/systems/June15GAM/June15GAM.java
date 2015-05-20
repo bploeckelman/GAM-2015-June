c74d2299b65af443943a91d3143db855c3e93b8b
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2015-05-19 23:39:25 -0500

    Add tween engine and accessors

--- core/src/com/lando/systems/June15GAM/June15GAM.java
+++ core/src/com/lando/systems/June15GAM/June15GAM.java
@@ -1,15 +1,30 @@
 package com.lando.systems.June15GAM;
 
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenManager;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.lando.systems.June15GAM.accessors.*;
 import com.lando.systems.June15GAM.screens.GameplayScreen;
 import com.lando.systems.June15GAM.screens.TestScreen;
 
 public class June15GAM extends Game {
+    public static TweenManager tween;
+
     TestScreen testScreen;
 
     @Override
     public void create() {
+        tween = new TweenManager();
+        Tween.registerAccessor(Color.class, new ColorAccessor());
+        Tween.registerAccessor(Rectangle.class, new RectangleAccessor());
+        Tween.registerAccessor(Vector2.class, new Vector2Accessor());
+        Tween.registerAccessor(Vector3.class, new Vector3Accessor());
+
         // TODO make this main menu later
         setScreen(new GameplayScreen());
     }
@@ -17,7 +32,7 @@ public class June15GAM extends Game {
     @Override
     public void render(){
         float dt = Gdx.graphics.getDeltaTime();
-        // TODO add tween manager here.
+        tween.update(dt);
 
         super.render();
     }
