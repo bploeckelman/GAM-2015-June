1c052f64667cbec394f7b70e53a0c448ffb0eb16
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2015-06-26 22:18:32 -0500

    Swank up the menu screen

--- android/assets/shaders/menu.frag
+++ android/assets/shaders/menu.frag
@@ -11,14 +11,18 @@ uniform vec2      u_resolution;
 void main() {
 	vec4 texColor = texture2D(u_texture, v_texCoord0);
 
-	float squares = 0.05;
+	float squares = 0.1;
+	//float squares = 0.05;
 	vec2 uv = v_texCoord0.xy;
 	uv.xy -= u_time / 100.;
 	vec2 moduv = mod(uv, squares) - (squares/2.);
 
+	//float dist = abs(moduv.y) + abs(moduv.x);
 	float dist = abs(moduv.x) + abs(moduv.y);
 	if (dist < squares/2.)
-		gl_FragColor = vec4(.35, .44, 0.75 + 0.25 * sin(u_time), 1.0);
+		gl_FragColor = vec4(.05, .5 + 0.25 * sin(3.0 * u_time), 0.05, 1.0);
+		//gl_FragColor = vec4(.35, .44, 0.75 + 0.25 * sin(u_time), 1.0);
 	else
-		gl_FragColor = vec4(.129, .224, .70 + 0.10 * sin(uv.y * 10. + 2. * u_time), 1.0);
+		gl_FragColor = vec4(.129, .6 + 0.10 * sin(uv.y * 10. + 3.0 * u_time), .129, 1.0);
+		//gl_FragColor = vec4(.129, .224, .70 + 0.10 * sin(uv.y * 10. + 2. * u_time), 1.0);
 }
--- core/src/com/lando/systems/June15GAM/screens/MenuScreen.java
+++ core/src/com/lando/systems/June15GAM/screens/MenuScreen.java
@@ -1,5 +1,11 @@
 package com.lando.systems.June15GAM.screens;
 
+import aurelienribon.tweenengine.BaseTween;
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenCallback;
+import aurelienribon.tweenengine.equations.Bounce;
+import aurelienribon.tweenengine.equations.Circ;
+import aurelienribon.tweenengine.equations.Quint;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
@@ -8,8 +14,10 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.lando.systems.June15GAM.Assets;
 import com.lando.systems.June15GAM.June15GAM;
+import com.lando.systems.June15GAM.accessors.Vector2Accessor;
 
 /**
  * Brian Ploeckelman created on 6/25/2015.
@@ -21,12 +29,16 @@ public class MenuScreen extends ScreenAdapter {
     OrthographicCamera camera;
     SpriteBatch        batch;
     BitmapFont         font;
-    GlyphLayout        layout;
+    String             line1;
+    String             line2;
+    Vector2            line1Pos;
+    Vector2            line2Pos;
+    boolean            introDone;
 
     float u_time;
 
-    String line1;
-    String line2;
+    private final float LINE_1_SCALE = 5f;
+    private final float LINE_2_SCALE = 1f;
 
     public MenuScreen(June15GAM game) {
         this.game = game;
@@ -35,10 +47,32 @@ public class MenuScreen extends ScreenAdapter {
         this.camera.update();
         this.batch = new SpriteBatch();
         this.font = new BitmapFont(Gdx.files.internal("fonts/2lines.fnt"));
-        this.layout = new GlyphLayout();
         this.line1 = "Bulwark!";
         this.line2 = "touch to play...";
+        this.line1Pos = new Vector2();
+        this.line2Pos = new Vector2();
         this.u_time = 0f;
+        this.introDone = false;
+
+        font.getData().setScale(LINE_1_SCALE);
+        final GlyphLayout line1Layout = new GlyphLayout(font, line1);
+        final float line1TargetX = (camera.viewportWidth - line1Layout.width) / 2f   - 15f;
+        final float line1TargetY = (camera.viewportHeight / 2f) + line1Layout.height - 15f;
+        line1Pos.x = line1TargetX;
+        line1Pos.y = camera.viewportHeight;
+        Tween.to(line1Pos, Vector2Accessor.Y, 2f)
+             .target(line1TargetY)
+             .ease(Circ.OUT)
+             .setCallback(new TweenCallback() {
+                 @Override
+                 public void onEvent(int i, BaseTween<?> baseTween) {
+                     introDone = true;
+                     startRepeatingTweens();
+                 }
+             })
+             .start(June15GAM.tween);
+
+        // TODO: fade in line2
     }
 
     public void update(float delta) {
@@ -71,20 +105,45 @@ public class MenuScreen extends ScreenAdapter {
 
         batch.setShader(null);
         batch.begin();
-        font.getData().setScale(5f);
-        layout.setText(font, line1);
-        font.draw(batch, line1, (camera.viewportWidth - layout.width) / 2f, (camera.viewportHeight / 2f) + layout.height);
-        final float lineHeight = (camera.viewportHeight + layout.height) / 2f - (layout.height / 2f);
-
-        font.getData().setScale(1f);
-        layout.setText(font, line2);
-        font.draw(batch, line2, (camera.viewportWidth - layout.width) / 2f, lineHeight);
+        font.getData().setScale(LINE_1_SCALE);
+        font.draw(batch, line1, line1Pos.x, line1Pos.y);
+        if (introDone) {
+            font.getData().setScale(LINE_2_SCALE);
+            font.draw(batch, line2, line2Pos.x, line2Pos.y);
+        }
         batch.end();
     }
 
     public void dispose() {
         font.dispose();
         batch.dispose();
+    }
+
+    private void startRepeatingTweens() {
+        final float TWEEN_SPEED = 0.5f;
+
+        font.getData().setScale(LINE_1_SCALE);
+        final GlyphLayout line1Layout = new GlyphLayout(font, line1);
+        final float line1TargetX = (camera.viewportWidth - line1Layout.width) / 2f;
+        final float line1TargetY = (camera.viewportHeight / 2f) + line1Layout.height;
+        line1Pos.x = line1TargetX - 15f;
+        line1Pos.y = line1TargetY - 15f;
+        Tween.to(line1Pos, Vector2Accessor.XY, TWEEN_SPEED)
+             .target(line1TargetX, line1TargetY)
+             .ease(Bounce.OUT)
+             .repeatYoyo(-1, TWEEN_SPEED)
+             .start(June15GAM.tween);
+
+        font.getData().setScale(LINE_2_SCALE);
+        final GlyphLayout line2Layout = new GlyphLayout(font, line2);
+        final float line2TargetX = (camera.viewportWidth  + line2Layout.width) / 2f - 10f;
+        line2Pos.x = line2TargetX + 150f;
+        line2Pos.y =  camera.viewportHeight / 2f;
+        Tween.to(line2Pos, Vector2Accessor.X, TWEEN_SPEED)
+             .target(line2TargetX)
+             .ease(Quint.OUT)
+             .repeatYoyo(-1, TWEEN_SPEED)
+             .start(June15GAM.tween);
     }
 
 }
